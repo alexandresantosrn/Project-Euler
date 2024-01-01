0e69176a0ae9c422ea82975d6ceba9d06b9af6be
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2024-01-01 11:39:42 -0300

    Finishing Question 6.

---
+++ Project-Euler/src/core/Problem006.java
@@ -0,0 +1,45 @@
+package core;
+
+public class Problem006 {
+
+	/*
+	 * The sum of the squares of the first ten natural numbers is, 1² + 2² + ... +
+	 * 10² = 385. The square of the sum of the first ten natural numbers is, (1 + 2
+	 * + ... + 10)² = 55² = 3025. Hence the difference between the sum of the
+	 * squares of the first ten natural numbers and the square of the sum is 3025 -
+	 * 385 = 2640.
+	 * 
+	 * Find the difference between the sum of the squares of the first one hundred
+	 * natural numbers and the square of the sum.
+	 * 
+	 * # Solved by: Alexandre Dantas - December 2023.
+	 */
+
+	public static void main(String[] args) {
+
+		int dif = findDifference();
+		System.out.println("Result: " + dif);
+	}
+
+	/**
+	 * Return the difference between the sum of the squares and the square of the
+	 * sum for number 100.
+	 * 
+	 * @return
+	 */
+	private static int findDifference() {
+
+		int sumSquares = 0;
+		int sum = 0;
+
+		for (int i = 1; i <= 100; i++) {
+			sumSquares += (i * i);
+			sum += i;
+		}
+
+		int squaresSum = sum * sum;
+		int difference = squaresSum - sumSquares;
+
+		return difference;
+	}
+}
