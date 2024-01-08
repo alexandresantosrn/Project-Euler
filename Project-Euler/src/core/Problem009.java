6f8e34cf85fd8979421c5c5286907f8e194fd543
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2024-01-08 09:54:04 -0300

    Starting Question 9.

---
+++ Project-Euler/src/core/Problem009.java
@@ -0,0 +1,43 @@
+package core;
+
+public class Problem009 {
+
+	public static void main(String[] args) {
+
+		int prod = findProdPythagorean();
+		System.out.println(prod);
+	}
+
+	private static int findProdPythagorean() {
+
+		int a;
+		int b;
+		int c;
+		double d;
+		double partial;
+		boolean find = false;
+
+		while (!find) {
+
+			for (int i = 0; i < 1000; i++) {
+				for (int j = 0; j < 1000; j++) {
+					c = 1000 - i - j;
+
+					if (c <= 0) {
+						break;
+					}
+					
+					partial = (c * c) + (i * i) + (j * j);
+					
+					d = Math.sqrt(partial);
+					
+					System.out.println(d);
+				}
+			}
+
+		
+		}
+		return 0;
+	}
+}	
+
